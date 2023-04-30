dacdd42c83dea57dbdfc7c02a1f6fce5a6e364a3
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-04-30 08:56:07 +0530

    Cleanup

--- client/src/components/Buttons.tsx
+++ client/src/components/Buttons.tsx
@@ -1,17 +1,22 @@
-import type { Component, JSXElement} from "solid-js"
+import type { Component, JSX, JSXElement} from "solid-js"
 
 export const Button: Component<{
   children: JSXElement | JSXElement[]
   onClick: () => void
+  id?: string | number
   variant?: "primary" | "secondary"
+  type?: "button" | "submit" | "reset"
+  role?: JSX.AriaAttributes["role"]
+  tabindex: number
 }> = (props) => {
   return (
     <button
+      role={props.role || 'button'}
       class={ props.variant === 'secondary' ?  
             'bg-white border  text-gray-500 rounded-md py-2 px-4 font-medium hover:text-gray-950 hover:bg-gray-200 hover:border-primary focus:bg-gray-200' 
             : 'bg-primary text-white rounded-md py-2 px-4 font-medium hover:bg-primary-dark'}
       onClick={props.onClick}
-      type="button"
+      type={props.type || 'button'}
     >
       {props.children} 
     </button>
