a83f2322393fbf7411eb80edad54bf3d6cee5a75
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-05-05 19:36:42 +0530

    review changes

--- client/src/components/Inputs/SelectInput.tsx
+++ client/src/components/Inputs/SelectInput.tsx
@@ -1,8 +1,13 @@
 import { Component, For } from "solid-js";
 
+interface Options {
+  label?: string
+  value: string
+}
+
 interface SelectInuputProps{
   label?: string
-  options: string[]
+  options: (Options | string)[] 
   onChange: (value: string | undefined) => void
 }
 
@@ -12,18 +17,28 @@ const SelectInput: Component<SelectInuputProps> = props => {
     const target = event.target as HTMLSelectElement;
     props.onChange(target.value);
   }
-
+  
   return (
   <label aria-label={`${props.label}-select-label`}>{props.label}
     <select name="choice" class="bg-gray-200 text-gray-700 border border-gray-300 rounded-lg w-64 py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 p-4 mx-2" 
             onChange={handleOptionChange}
     >
       <For each={props.options}>
-        {(option, index) => (
-          <option value={option} id={`${index}-${option}`}>
-            {option}
-          </option>
-        )}
+        {(option, index) => {
+          if (typeof option === 'string') {
+            return (
+              <option value={option}>
+                {option}
+              </option>
+            )
+          } else {
+            return (
+              <option value={option.value}>
+                {option.label || option.value}
+              </option>
+            )
+          }
+        }}
       </For>
     </select>
   </label>  
